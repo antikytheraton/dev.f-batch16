84654d191a3bf2a70d80f92ceaa0dea96a096f76
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2017-09-05 20:26:15 -0500

    StarWars-API

---
+++ week2/swdex/Characters/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+import requests
+# Create your views here.
+
+def index(request,pagina=1):
+    req = requests.get("http://swapi.co/api/people/?page={0}".format(pagina))
+    if req.status_code == 200:
+        characters = req.json()['results']
+    else:
+        characters = []
+    return render(request,'index.html',{'personajes':characters})
